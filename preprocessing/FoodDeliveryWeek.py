f9cdfc4f784e17712b194b1537448283fd2abfd5
NaroaCS <naroa.coretti@gmail.com>
2023-06-27 17:27:22 -0400

    demand plotting week

---
+++ preprocessing/FoodDeliveryWeek.py
@@ -0,0 +1,33 @@
+import pandas as pd
+from datetime import timedelta
+
+# Read the original CSV file
+df_week = pd.read_csv('preprocessing/data/fooddeliverytrips_cambridge.csv')
+df_weekend = pd.read_csv('preprocessing/data/fooddeliverytrips_cambridge_sat.csv')
+
+df1=df_week.copy()
+df1['day'] = 1
+
+df2=df_week.copy()
+df2['day'] = 2
+
+df3=df_week.copy()
+df3['day'] = 3
+
+df4=df_week.copy()
+df4['day'] = 4
+
+df5=df_week.copy()
+df5['day'] = 5
+
+df6=df_weekend.copy()
+df6['day'] = 6
+
+df7=df_weekend.copy()
+df7['day'] = 7
+
+# Concat the data
+df_total = pd.concat([df1,df2,df3,df4,df5,df6,df7], ignore_index=True)
+
+# Writedata to a new CSV file
+df_total.to_csv('includes/Demand/food_demand_cambridge_week.csv', index=False)
